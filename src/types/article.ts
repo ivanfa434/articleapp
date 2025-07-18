3ad3b6720ca95044976e1cec5b079d45c30fb45e
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-07-18 20:10:10 +0700

    initial init

---
+++ src/types/article.ts
@@ -0,0 +1,14 @@
+import { Category } from "./category";
+import { User } from "./user";
+
+export interface Article {
+  id: string;
+  title: string;
+  content: string;
+  userId: string;
+  categoryId: string;
+  createdAt: string;
+  updatedAt: string;
+  category: Category;
+  user: User;
+}
